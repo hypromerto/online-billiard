1e7f21cfec1d84acdd4862a6dcf9882c816d620b
mert <aslan-mert99@hotmail.com>
2022-03-26 04:36:50 +0300

    Initial commit

---
+++ Source/Bilardo/BilardoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BilardoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABilardoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABilardoGameMode();
+};
+
+
+
